d3a3b3163ccc2fe7be7d0802a72c1bbc7805bcff
Pedre <pviljoen@fullfacing.com>
2018-05-18 11:56:54 +0200

    Init commit

---
+++ cloudMQ.js
@@ -0,0 +1,125 @@
+// This file will connect to a cloudAMQP connection,
+// post a message to the exchange, and listen on a queue
+
+// ampq dependancy
+var amqp = require('amqplib/callback_api')
+var ampqConn = null
+const constant = require('./constant')
+
+function start() {
+  amqp.connect(constant.amqp_url + "?heartbeat=60", function(err, conn) {
+    if (err) {
+      console.error("[AMQP]", err.message)
+      return setTimeout(start, 1000)
+    }
+    conn.on("error", function(err) {
+      if (err.message !== "Connection closing") {
+        console.error("[AMQP] conn error", err.message)
+      }
+    })
+    conn.on("close", function() {
+      console.error("[AMQP] reconnecting")
+      return setTimeout(start, 1000)
+    })
+    console.log("[AMQP] connected")
+    amqpConn = conn
+    whenConnected()
+  })
+}
+
+function whenConnected(){
+  startPublisher()
+  startWorker()
+}
+
+// start publisher
+var pubChannel = null
+var offlinePubQueue = []
+function startPublisher() {
+  amqpConn.createConfirmChannel(function(err, ch) {
+    if (closeOnErr(err)) return
+      ch.on("error", function(err) {
+      console.error("[AMQP] channel error", err.message)
+    })
+    ch.on("close", function() {
+      console.log("[AMQP] channel closed")
+    });
+
+    pubChannel = ch
+    while (true) {
+      var m = offlinePubQueue.shift()
+      if (!m) break
+      publish(m[0], m[1], m[2])
+    }
+  });
+}
+
+// publish
+function publish(exchange, routingKey, content) {
+  try {
+    pubChannel.publish(exchange, routingKey, content, { persistent: true },
+                      function(err, ok) {
+                        if (err) {
+                          console.error("[AMQP] publish", err)
+                          offlinePubQueue.push([exchange, routingKey, content])
+                          pubChannel.connection.close()
+                        }
+                      })
+  } catch (e) {
+    console.error("[AMQP] publish", e.message)
+    offlinePubQueue.push([exchange, routingKey, content])
+  }
+}
+
+// Consume messages
+// A worker that acks messages only if processed successfully
+function startWorker() {
+  amqpConn.createChannel(function(err, ch) {
+    if (closeOnErr(err)) return
+    ch.on("error", function(err) {
+      console.error("[AMQP] channel error", err.message)
+    })
+    ch.on("close", function() {
+      console.log("[AMQP] channel closed")
+    })
+
+    ch.prefetch(10)
+    ch.assertQueue("jobs", { durable: true }, function(err, _ok) {
+      if (closeOnErr(err)) return;
+      ch.consume("jobs", processMsg, { noAck: false })
+      console.log("Worker is started")
+    })
+
+    function processMsg(msg) {
+      work(msg, function(ok) {
+        try {
+          if (ok)
+            ch.ack(msg)
+          else
+            ch.reject(msg, true)
+        } catch (e) {
+          closeOnErr(e)
+        }
+      })
+    }
+  })
+}
+
+function work(msg, cb) {
+  console.log("Message content: ", msg.content.toString())
+  cb(true)
+}
+
+// close the connection on error
+function closeOnErr(err) {
+  if (!err) return false
+  console.error("[AMQP] error", err)
+  amqpConn.close()
+  return true
+}
+
+setInterval(function() {
+  publish("", "jobs", new Buffer("Hello Tiaan"))
+}, 1000)
+
+start()
